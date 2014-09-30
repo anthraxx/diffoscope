27a7d0279f52b1404b47c73d4bd9470d54bab1ff
Jérémy Bobbio <lunar@debian.org>
2014-09-30 19:30:34 +0200

    Add setup.py
    Thanks to Hans-Christoph Steiner.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+
+from setuptools import setup, find_packages
+
+setup(name='debbindiff',
+      version='2',
+      description='display differences between files',
+      long_description=open('README').read(),
+      author='Lunar',
+      author_email='lunar@debian.org',
+      url='https://wiki.debian.org/ReproducibleBuilds',
+      packages=find_packages(),
+      scripts=['debbindiff.py'],
+      install_requires=[
+          'python-debian',
+          'magic',
+          ],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Operating System :: POSIX',
+          'Topic :: Utilities',
+          ],
+      )

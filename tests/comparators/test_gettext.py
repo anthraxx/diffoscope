0e221d1a7b49e91574f6eca012a1678f67f885ef
Jérémy Bobbio <lunar@debian.org>
2015-06-24 13:26:40 +0000

    Add tests for Gettext comparators

---
+++ tests/comparators/test_gettext.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# debbindiff: highlight differences between two builds of Debian packages
+#
+# Copyright © 2015 Jérémy Bobbio <lunar@debian.org>
+#
+# debbindiff is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# debbindiff is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with debbindiff.  If not, see <http://www.gnu.org/licenses/>.
+
+import codecs
+import os.path
+import shutil
+import pytest
+from debbindiff.comparators.gettext import compare_mo_files
+
+TEST_FILE1_PATH = os.path.join(os.path.dirname(__file__), '../data/test1.mo') 
+TEST_FILE2_PATH = os.path.join(os.path.dirname(__file__), '../data/test2.mo') 
+
+def test_no_differences():
+    differences = compare_mo_files(TEST_FILE1_PATH, TEST_FILE1_PATH)
+    assert len(differences) == 0
+
+@pytest.fixture
+def differences():
+    return compare_mo_files(TEST_FILE1_PATH, TEST_FILE2_PATH)[0].details # skip container with path
+
+def test_diff(differences):
+    expected_diff = open(os.path.join(os.path.dirname(__file__), '../data/mo_expected_diff')).read()
+    assert differences[0].unified_diff == expected_diff
+
+def test_charsets():
+    differences = compare_mo_files(os.path.join(os.path.dirname(__file__), '../data/test_no_charset.mo'),
+                                   os.path.join(os.path.dirname(__file__), '../data/test_iso8859-1.mo'))
+    differences = differences[0].details # skip container with path
+    expected_diff = codecs.open(os.path.join(os.path.dirname(__file__), '../data/mo_charsets_expected_diff'), encoding='utf-8').read()
+    assert differences[0].unified_diff == expected_diff

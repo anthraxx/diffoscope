a437d366d54c6e79beb84f0bca92c821cac93fd0
Chris Lamb <lamby@debian.org>
2015-08-21 08:22:02 +0200

    Correct "comment" on Device comparator.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- diffoscope/comparators/device.py
+++ diffoscope/comparators/device.py
@@ -50,4 +50,4 @@ class Device(File):
         logger.debug('my_content %s', self.path)
         with open(self.path) as my_content, \
              open(other.path) as other_content:
-            return Difference.from_file(my_content, other_content, self.name, other.name, source=source, comment="symlink")
+            return Difference.from_file(my_content, other_content, self.name, other.name, source=source, comment="device")

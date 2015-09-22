dd53287d4c4025fe9a53145b401d343f57aaa999
Jérémy Bobbio <lunar@debian.org>
2015-09-22 11:24:54 +0200

    Release version 36

--- diffoscope/__init__.py
+++ diffoscope/__init__.py
@@ -22,7 +22,7 @@ import logging
 from distutils.spawn import find_executable
 import os
 
-VERSION = "35"
+VERSION = "36"
 
 logger = logging.getLogger("diffoscope")
 logger.setLevel(logging.WARNING)

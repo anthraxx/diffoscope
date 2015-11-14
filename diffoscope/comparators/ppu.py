c5bfa7343bc00afab739c31168e8a93517d7bff8
Jérémy Bobbio <lunar@debian.org>
2015-11-14 14:05:19 +0100

    Force ppudump to output time in UTC
    ppudump will return times using the local timezone which is not ideal to
    investigate files.
    
    Sadly, it doesn't support the TZ environment variable… but it looks for
    timezone definitions in the directory specified by TZDIR.
    
    So let's set it to a non-existent directory so we get UTC output even when the
    system timezone is set otherwise. The expected diff in the test suite is
    updated to reflect that.

--- diffoscope/comparators/ppu.py
+++ diffoscope/comparators/ppu.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with diffoscope.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import re
 from diffoscope import tool_required
 from diffoscope.comparators.binary import File, needs_content
@@ -30,6 +31,16 @@ class Ppudump(Command):
     @tool_required('ppudump')
     def cmdline(self):
         return ['ppudump', self.path]
+
+    def env(self):
+        # ppudump will return times using the local timezone which is not ideal
+        # to investigate files. Sadly, it doesn't support the TZ environment
+        # variable… but it looks for timezone definitions in the directory
+        # specified by TZDIR. So let's set it to a non-existent directory
+        # so we get UTC output even when the system timezone is set otherwise.
+        env = dict(os.environ)
+        env['TZDIR'] = '/nonexistent'
+        return env
 
     def filter(self, line):
         if re.match(r'^Analyzing %s \(v[0-9]+\)$' % re.escape(self.path), line.decode('utf-8')):

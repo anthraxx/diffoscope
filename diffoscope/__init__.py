031cf938af133744b171d41316499f5dedfeb224
Chris Lamb <lamby@debian.org>
2016-08-01 17:34:13 -0400

    Tidy PROVIDERS dict in diffoscope/__init__.py

--- diffoscope/__init__.py
+++ diffoscope/__init__.py
@@ -41,92 +41,163 @@ OS_NAMES = { 'arch': 'Arch Linux'
 
 class RequiredToolNotFound(Exception):
     PROVIDERS = {
-                  'bzip2':      { 'debian': 'bzip2'
-                                , 'arch': 'bzip2' }
-                , 'cbfstool':   {}
-                , 'cd-iccdump': { 'debian': 'colord'
-                                , 'arch': 'colord'
-                                , 'FreeBSD': 'colord' }
-                , 'cmp':        { 'debian': 'diffutils'
-                                , 'arch': 'diffutils' }
-                , 'cpio':       { 'debian': 'cpio'
-                                , 'arch': 'cpio' }
-                , 'diff':       { 'debian': 'diffutils'
-                                , 'arch': 'diffutils' }
-                , 'enjarify':   { 'debian': 'enjarify'
-                                , 'arch': 'enjarify' }
-                , 'file':       { 'debian': 'file'
-                                , 'arch': 'file' }
-                , 'find':       { 'debian': 'findutils'
-                                , 'arch': 'findutils' }
-                , 'getfacl':    { 'debian': 'acl'
-                                , 'arch': 'acl' }
-                , 'ghc':        { 'debian': 'ghc'
-                                , 'arch': 'ghc'
-                                , 'FreeBSD': 'ghc' }
-                , 'gpg':        { 'debian': 'gnupg'
-                                , 'arch': 'gnupg'
-                                , 'FreeBSD': 'gnupg' }
-                , 'gzip':       { 'debian': 'gzip'
-                                , 'arch': 'gzip' }
-                , 'img2txt':    { 'debian': 'caca-utils'
-                                , 'arch': 'libcaca'
-                                , 'FreeBSD': 'libcaca' }
-                , 'isoinfo':    { 'debian': 'genisoimage'
-                                , 'arch': 'cdrkit'
-                                , 'FreeBSD': 'cdrtools' }
-                , 'javap':      { 'debian': 'default-jdk | java-sdk'
-                                , 'arch': 'java-environment' }
-                , 'ls':         { 'debian': 'coreutils'
-                                , 'arch': 'coreutils' }
-                , 'lsattr':     { 'debian': 'e2fsprogs'
-                                , 'arch': 'e2fsprogs'
-                                , 'FreeBSD': 'e2fsprogs'}
-                , 'msgunfmt':   { 'debian': 'gettext'
-                                , 'arch': 'gettext'
-                                , 'FreeBSD': 'gettext-tools' }
-                , 'objdump':    { 'debian': 'binutils-multiarch'
-                                , 'arch': 'binutils' }
-                , 'pdftk':      { 'debian': 'pdftk'
-                                , 'FreeBSD': 'pdftk' }
-                , 'pdftotext':  { 'debian': 'poppler-utils'
-                                , 'arch': 'poppler'
-                                , 'FreeBSD': 'poppler-utils' }
-                , 'pedump':     { 'debian': 'mono-utils'
-                                , 'arch': 'mono-tools'
-                                , 'FreeBSD': 'mono' }
-                , 'ppudump':    { 'debian': 'fp-utils'
-                                , 'arch': 'fpc'
-                                , 'FreeBSD': 'fpc' }
-                , 'ps2ascii':   { 'debian': 'ghostscript'
-                                , 'arch': 'ghostscript'
-                                , 'FreeBSD': 'ghostscript9-base' }
-                , 'readelf':    { 'debian': 'binutils-multiarch'
-                                , 'arch': 'binutils' }
-                , 'rpm2cpio':   { 'debian': 'rpm2cpio'
-                                , 'arch': 'rpmextract'
-                                , 'FreeBSD': 'rpm2cpio' }
-                , 'showttf':    { 'debian': 'fontforge-extras' }
-                , 'sng':        { 'debian': 'sng' }
-                , 'stat':       { 'debian': 'coreutils'
-                                , 'arch': 'coreutils' }
-                , 'sqlite3':    { 'debian': 'sqlite3'
-                                , 'arch': 'sqlite'
-                                , 'FreeBSD': 'sqlite3' }
-                , 'tar':        { 'debian': 'tar'
-                                , 'arch': 'tar' }
-                , 'unsquashfs': { 'debian': 'squashfs-tools'
-                                , 'arch': 'squashfs-tools'
-                                , 'FreeBSD': 'squashfs-tools' }
-                , 'xxd':        { 'debian': 'vim-common'
-                                , 'arch': 'vim'
-                                , 'FreeBSD': 'vim | vim-lite' }
-                , 'xz':         { 'debian': 'xz-utils'
-                                , 'arch': 'xz' }
-                , 'zipinfo':    { 'debian': 'unzip'
-                                , 'arch': 'unzip'
-                                , 'FreeBSD': 'unzip' }
-                }
+        'bzip2': {
+            'debian': 'bzip2',
+            'arch': 'bzip2',
+        },
+        'cbfstool': {
+        },
+        'cd-iccdump': {
+            'debian': 'colord',
+            'arch': 'colord',
+            'FreeBSD': 'colord',
+        },
+        'cmp': {
+            'debian': 'diffutils',
+            'arch': 'diffutils',
+        },
+        'cpio': {
+            'debian': 'cpio',
+            'arch': 'cpio',
+        },
+        'diff': {
+            'debian': 'diffutils',
+            'arch': 'diffutils',
+        },
+        'enjarify': {
+            'debian': 'enjarify',
+            'arch': 'enjarify',
+        },
+        'file': {
+            'debian': 'file',
+            'arch': 'file',
+        },
+        'find': {
+            'debian': 'findutils',
+            'arch': 'findutils',
+        },
+        'getfacl': {
+            'debian': 'acl',
+            'arch': 'acl',
+        },
+        'ghc': {
+            'debian': 'ghc',
+            'arch': 'ghc',
+            'FreeBSD': 'ghc',
+        },
+        'gpg': {
+            'debian': 'gnupg',
+            'arch': 'gnupg',
+            'FreeBSD': 'gnupg',
+        },
+        'gzip': {
+            'debian': 'gzip',
+            'arch': 'gzip',
+        },
+        'img2txt': {
+            'debian': 'caca-utils',
+            'arch': 'libcaca',
+            'FreeBSD': 'libcaca',
+        },
+        'isoinfo': {
+            'debian': 'genisoimage',
+            'arch': 'cdrkit',
+            'FreeBSD': 'cdrtools',
+        },
+        'javap': {
+            'debian': 'default-jdk | java-sdk',
+            'arch': 'java-environment',
+        },
+        'ls': {
+            'debian': 'coreutils',
+            'arch': 'coreutils',
+        },
+        'lsattr': {
+            'debian': 'e2fsprogs',
+            'arch': 'e2fsprogs',
+            'FreeBSD': 'e2fsprogs',
+        },
+        'msgunfmt': {
+            'debian': 'gettext',
+            'arch': 'gettext',
+            'FreeBSD': 'gettext-tools',
+        },
+        'objdump': {
+            'debian': 'binutils-multiarch',
+            'arch': 'binutils',
+        },
+        'pdftk': {
+            'debian': 'pdftk',
+            'FreeBSD': 'pdftk',
+        },
+        'pdftotext': {
+            'debian': 'poppler-utils',
+            'arch': 'poppler',
+            'FreeBSD': 'poppler-utils',
+        },
+        'pedump': {
+            'debian': 'mono-utils',
+            'arch': 'mono-tools',
+            'FreeBSD': 'mono',
+        },
+        'ppudump': {
+            'debian': 'fp-utils',
+            'arch': 'fpc',
+            'FreeBSD': 'fpc',
+        },
+        'ps2ascii': {
+            'debian': 'ghostscript',
+            'arch': 'ghostscript',
+            'FreeBSD': 'ghostscript9-base',
+        },
+        'readelf': {
+            'debian': 'binutils-multiarch',
+            'arch': 'binutils',
+        },
+        'rpm2cpio': {
+            'debian': 'rpm2cpio',
+            'arch': 'rpmextract',
+            'FreeBSD': 'rpm2cpio',
+        },
+        'showttf': {
+            'debian': 'fontforge-extras',
+        },
+        'sng': {
+            'debian': 'sng',
+        },
+        'stat': {
+            'debian': 'coreutils',
+            'arch': 'coreutils',
+        },
+        'sqlite3': {
+            'debian': 'sqlite3',
+            'arch': 'sqlite',
+            'FreeBSD': 'sqlite3',
+        },
+        'tar': {
+            'debian': 'tar',
+            'arch': 'tar',
+        },
+        'unsquashfs': {
+            'debian': 'squashfs-tools',
+            'arch': 'squashfs-tools',
+            'FreeBSD': 'squashfs-tools',
+        },
+        'xxd': {
+            'debian': 'vim-common',
+            'arch': 'vim',
+            'FreeBSD': 'vim | vim-lite',
+        },
+        'xz': {
+            'debian': 'xz-utils',
+            'arch': 'xz',
+        },
+        'zipinfo': {
+            'debian': 'unzip',
+            'arch': 'unzip',
+            'FreeBSD': 'unzip',
+        },
+    }
 
     def __init__(self, command):
         self.command = command

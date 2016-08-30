6984965583fe2d90f1ba2a3afd349067b128d9f1
Mattia Rizzolo <mattia@debian.org>
2016-08-30 13:36:51 +0000

    add copyright/license header

--- diffoscope/exc.py
+++ diffoscope/exc.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# diffoscope: in-depth comparison of files, archives, and directories
+#
+# Copyright © 2014-2015 Jérémy Bobbio <lunar@debian.org>
+#             2016      Chris Lamb <lamby@debian.org>
+#
+# diffoscope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffoscope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffoscope.  If not, see <http://www.gnu.org/licenses/>.
+
 class OutputParsingError(Exception):
     def __init__(self, command, object):
         self.command = command

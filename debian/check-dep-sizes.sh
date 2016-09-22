6aaf035572a2c327f9a310aa07306829f9560fb2
Ximin Luo <infinity0@debian.org>
2016-09-22 11:48:24 +0200

    debian/check-dep-sizes: fix query to cover all depth {1,2} {depends, recommends} relationships

--- debian/check-dep-sizes.sh
+++ debian/check-dep-sizes.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # Check sizes of diffoscope's dependencies (including recommends)
 # Give ~i to only list packages installed on your system.
-LC_ALL=C aptitude search '~Rrecommends:diffoscope|~R~Rrecommends:diffoscope'" $1" \
+pkg=diffoscope
+d=~R
+r=~Rrecommends:
+LC_ALL=C aptitude search "($d$pkg|$r$pkg|$d$d$pkg|$d$r$pkg|$r$d$pkg|$r$r$pkg) $1" \
   --disable-columns -F '%I %p' \
   | sed -e 's/ kB / KB /g' \
   | LC_ALL=C sort -k2,2 -k1n,1n \

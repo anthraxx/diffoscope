f4fdf12ee5f371ccaa28fe973e6710ae123f6d4f
Chris Lamb <lamby@debian.org>
2016-12-15 22:31:39 +0100

    Improve documentation of new tests/conftest.py file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,25 @@
-import diffoscope
-import pytest
+# -*- coding: utf-8 -*-
+#
+# diffoscope: in-depth comparison of files, archives, and directories
+#
+# Copyright © 2016 Brett Smith <debbug@brettcsmith.org>
+#
+# diffoscope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffoscope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffoscope.  If not, see <http://www.gnu.org/licenses/>.
 
+import pytest
+import diffoscope
+
+
+# Ensure set_locale fixture runs before all tests.
 set_locale = pytest.fixture(autouse=True, scope='session')(diffoscope.set_locale)

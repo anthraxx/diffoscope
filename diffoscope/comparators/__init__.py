6fedebd28232632e8dd8b5711a2983370f29fe2c
Chris Lamb <lamby@debian.org>
2016-12-22 18:13:38 +0000

    Clarify what "same content" means in compare_files debug message.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- diffoscope/comparators/__init__.py
+++ diffoscope/comparators/__init__.py
@@ -134,7 +134,7 @@ def compare_root_paths(path1, path2):
 def compare_files(file1, file2, source=None):
     logger.debug("Comparing files %s and %s", file1, file2)
     if file1.has_same_content_as(file2):
-        logger.debug('same content, skipping')
+        logger.debug("has_same_content_as returned True; skipping further comparisons")
         return None
     specialize(file1)
     specialize(file2)

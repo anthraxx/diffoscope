4ef72d4c652e687926ef800f937220caaf835267
Chris Lamb <lamby@debian.org>
2017-01-15 22:58:23 +1100

    specialize: Tidy and add better logging.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- diffoscope/comparators/utils/specialize.py
+++ diffoscope/comparators/utils/specialize.py
@@ -31,22 +31,31 @@ def specialize(file):
     for cls in FILE_CLASSES:
         if isinstance(file, cls):
             return file
-        ret = False
+
+        # Does this file class match?
+        flag = False
         if hasattr(cls, 'recognizes'):
             with profile('recognizes', file):
-                ret = cls.recognizes(file)
+                flag = cls.recognizes(file)
         else:
             # No recognizes() method specified, try an auto recognition
             if hasattr(cls, 'RE_FILE_TYPE'):
-                ret = cls.RE_FILE_TYPE.search(file.magic_file_type)
+                flag = bool(cls.RE_FILE_TYPE.search(file.magic_file_type))
             elif hasattr(cls, 'RE_FILE_EXTENSION'):
-                ret = cls.RE_FILE_EXTENSION.search(file.name)
-        if ret:
-            logger.debug("Using %s for %s", cls.__name__, file.name)
-            new_cls = type(cls.__name__, (cls, type(file)), {})
-            file.__class__ = new_cls
-            return file
-    logger.debug('Unidentified file. Magic says: %s', file.magic_file_type)
+                flag = bool(cls.RE_FILE_EXTENSION.search(file.name))
+
+        if not flag:
+            continue
+
+        # Found a match; perform type magic
+        logger.debug("Using %s for %s", cls.__name__, file.name)
+        new_cls = type(cls.__name__, (cls, type(file)), {})
+        file.__class__ = new_cls
+
+        return file
+
+    logger.debug("Unidentified file. Magic says: %s", file.magic_file_type)
+
     return file
 
 def import_comparators(comparators):

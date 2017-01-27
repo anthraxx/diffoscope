anthraxx/diffoscope

commit 36d1c9647d0cc68a4413462dfa6cde69591f5418

Save some complicated logic by setting default RE_FILE_{EXTENSION,TYPE}
Signed-off-by: Chris Lamb <lamby@debian.org>

diffoscope/comparators/utils/file.py
@@ -40,6 +40,9 @@ logger = logging.getLogger(__name__)
 
 
 class File(object, metaclass=abc.ABCMeta):
+    RE_FILE_TYPE = re.compile(r'.*')
+    RE_FILE_EXTENSION = re.compile(r'.*')
+
     if hasattr(magic, 'open'): # use Magic-file-extensions from file
         @classmethod
         def guess_file_type(self, path):

diffoscope/comparators/utils/specialize.py
@@ -38,17 +38,8 @@ def specialize(file):
             with profile('recognizes', file):
                 flag = cls.recognizes(file)
         else:
-            # No recognizes() method specified, try an auto recognition
-            def search_type(file):
-                return bool(cls.RE_FILE_TYPE.search(file.magic_file_type))
-            def search_ext(file):
-                return bool(cls.RE_FILE_EXTENSION.search(file.name))
-            if hasattr(cls, 'RE_FILE_TYPE') and hasattr(cls, 'RE_FILE_EXTENSION'):
-                flag = search_type(file) and search_ext(file)
-            elif hasattr(cls, 'RE_FILE_TYPE'):
-                flag = search_type(file)
-            elif hasattr(cls, 'RE_FILE_EXTENSION'):
-                flag = search_ext(file)
+            flag = cls.RE_FILE_TYPE.search(file.magic_file_type) and \
+                cls.RE_FILE_EXTENSION.search(file.name)
 
         if not flag:
             continue

commit 4366e7bdb86c610dc6fbc371ef9467985f2040a1

Fix bug introduced in commit 36d1c964 that only worked "accidentally"
The previous behaviour (before the commit) returns False if neither regexes are
defined. It is not possible to reduce this to a simple boolean expression; one
needs to explicitly check for the corner-case "neither are defined". The
behaviour in the other cases was correct (both before and after the commit),
however the use of the "match-all" regexp makes it hard to express the corner
case. So, rewrite the whole thing.

diffoscope/comparators/utils/file.py
@@ -40,8 +40,8 @@ logger = logging.getLogger(__name__)
 
 
 class File(object, metaclass=abc.ABCMeta):
-    RE_FILE_TYPE = re.compile(r'.*')
-    RE_FILE_EXTENSION = re.compile(r'.*')
+    RE_FILE_TYPE = None
+    RE_FILE_EXTENSION = None
 
     if hasattr(magic, 'open'): # use Magic-file-extensions from file
         @classmethod

diffoscope/comparators/utils/specialize.py
@@ -38,8 +38,11 @@ def specialize(file):
             with profile('recognizes', file):
                 flag = cls.recognizes(file)
         else:
-            flag = cls.RE_FILE_TYPE.search(file.magic_file_type) and \
-                cls.RE_FILE_EXTENSION.search(file.name)
+            re_tests = [(cls.RE_FILE_TYPE, file.magic_file_type),
+                        (cls.RE_FILE_EXTENSION, file.name)]
+            re_tests = filter(lambda pair: pair[0], re_tests)
+            if re_tests: # if neither are defined, it's *not* a match
+                flag = all(bool(pair[0].search(pair[1])) for pair in re_tests)
 
         if not flag:
             continue
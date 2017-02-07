68504f2ec5c5ae8313a1329ebc31ffe646906d82
Chris Lamb <lamby@debian.org>
2017-02-07 14:07:17 +1300

    Drop debugging code from previous commit.
    Gbp-Dch: ignore
    
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- tests/test_presenters.py
+++ tests/test_presenters.py
@@ -99,9 +99,6 @@ def test_restructuredtext(capsys):
 def test_json(capsys):
     out = run(capsys, '--json', '-')
 
-    with open('tests/data/output.json', 'w') as f:
-        f.write(out)
-
     assert out == data('output.json')
 
 def test_no_report_option(capsys):

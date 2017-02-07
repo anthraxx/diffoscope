2e3a4a539801e87d636d96fd44c431966f3310fd
Chris Lamb <lamby@debian.org>
2017-02-08 12:46:29 +1300

    presenters.utils: Tidy and optimise create_limited_print_func
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- diffoscope/presenters/utils.py
+++ diffoscope/presenters/utils.py
@@ -75,14 +75,16 @@ def make_printer(path):
         output.close()
 
 def create_limited_print_func(print_func, max_page_size):
-    def limited_print_func(s, force=False):
-        if max_page_size == 0:
-            print_func(s)
+    count = 0
+
+    def fn(val, force=False, count=count):
+        print_func(val)
+
+        if force or max_page_size == 0:
             return
-        if not hasattr(limited_print_func, 'char_count'):
-            limited_print_func.char_count = 0
-        print_func(s)
-        limited_print_func.char_count += len(s)
-        if not force and limited_print_func.char_count >= max_page_size:
+
+        count += len(val)
+        if count >= max_page_size:
             raise PrintLimitReached()
-    return limited_print_func
+
+    return fn
